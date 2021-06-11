f73d3f802ccae8232d366a90efc6b94507f00b97
Charles <charleshparmley@icloud.com>
2021-06-10 20:32:23 -0500

    Refactored for Optimization

--- Hive/detection/views.py
+++ Hive/detection/views.py
@@ -14,65 +14,43 @@ class SettingsView(ListView):
     template_name = "home.html"
     def as_view():
         def graph_function(request):
-              
             #Variables to store objects from models
-            company_model = Company.objects.all()
-            movement_model = Movement.objects.all()
-            iterations = len(company_model)
-
-            # Using counter to access the company model
-            count = 0
-
+            company_model,movement_model,iterations = Company.objects.all(),Movement.objects.all(),len(Company.objects.all())
             # Contains objects needed to render data on page
             returned_objects_dictionary = {'object_list':company_model, 'entrances':movement_model}
-
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+            # The below loops need to be refactored to load all the movements into a variable containing a list of movements for each company.
+            # Then access the movements according to company name. Instead of iterating every single movement everytime the loop is entered.    
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+            # Using counter to access the company model
+            count = 0
             while count < int(iterations):
                 # These lists must be cleared of their data on each iteration
-                in_list =[]
-                out_list =[]
-                x=[]
-                y=[]
-                x2=[]
-                y2=[]
-
-#-----------------------------------------------------------------------------------------------------------------------------------------------------
-            # This needs to be refactored to load all the movements into a variable containing a list of movements for each company
-            # then access the movements according to company name. Instead of iterating every single movement everytime the loop is entered.    
-#-----------------------------------------------------------------------------------------------------------------------------------------------------
-                for item in movement_model:
-                    #if company name in the model is the one asked for
-                    company_model_name = company_model[count].company
-                    if company_model_name == item.company:
-
-                        # if the iterations entry time exists
-                        if item.timeOut:
-                            #add the the date/time to a list
-                            out_list.append(item.timeOut)
-                            #create a list and within it store it's hour
-                            x2 = [x2.hour for x2 in out_list]
-
-                        # if the iterations entry time exists
-                        if item.timeIn:
-                            #add the the date/time to a list
-                            in_list.append(item.timeIn)                        
-                            #create a list and within it store the hour
-                            x = [x.hour for x in in_list]
-                
+                in_list,out_list ,x ,y ,x2 ,y2 = [],[],[],[],[],[]
+                company_model_name = company_model[count].company
+                #if the company name of the company_model & movement_model is the same
+                movements =[item for item in movement_model if company_model_name == item.company]
+                count2=0
+                while count2 < len(movements):
+                    # if the iterations entry time exists
+                    if movements[count2].timeOut:
+                        out_list.append(movements[count2].timeOut)
+                        #create a list and within it store it's hour
+                        x2 = [x2.hour for x2 in out_list]
+                    # if the iterations entry time exists
+                    if movements[count2].timeIn:
+                        in_list.append(movements[count2].timeIn)                        
+                        #create a list and within it store the hour
+                        x = [x.hour for x in in_list]
+                    count2+=1
                 #count number of times entrences/exits happened per hour
-                for i in x:
-                    y.append(x.count(i))
-                for i in x2:
-                    y2.append(x2.count(i))
-
+                y,y2 = [x.count(i) for i in x],[x.count(i) for i in x2]
                 # Dynamic string creation
                 chart_name = "chart{}".format(count)
-
                 # Dynamic variable creation
                 globals()["chart{}".format(count)] = get_plot(x, y, x2, y2)
-
-                # Append Dynamic variable to the dictionary
+                # Append created variable to dictionary
                 returned_objects_dictionary[chart_name] = globals()["chart{}".format(count)]
-
                 count += 1
             return render(request, 'home.html', returned_objects_dictionary)   
         return graph_function
@@ -97,4 +75,3 @@ class CompanyDeleteView(DeleteView):
     model = Company
     template_name = 'company_reset.html'
     success_url = reverse_lazy('home')
-
